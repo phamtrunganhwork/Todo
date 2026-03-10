1fbfe4d48707bae49c1c1099fd4ca25ffe8c0968
phamtrunganhwork <143469756+phamtrunganhwork@users.noreply.github.com>
2026-03-10 22:49:30 +0700

    Rename functions, add ts

--- todo/app/page.js
+++ todo/app/page.js
@@ -11,13 +11,13 @@ export default function Page(){
   const [tasks, setTasks] = useState([{id:0, todo:"One"}]);
   const [idgiver, setIdgiver] = useState(1)
 
-  function addButtonHandle(){
+  function addButtonHandler(){
     setIdgiver(idgiver+1)
     setTasks([...tasks, {id: idgiver, todo: "New Task"}])
     console.log(tasks)
   }
 
-  function deleteButtonHandle(id){
+  function deleteButtonHandler(id){
     setTasks(tasks.filter((task) => task.id !== id))
     console.log(id)
   }
@@ -29,8 +29,8 @@ export default function Page(){
   return <>
   <Header/>
   {console.log(tasks)}
-  { tasks ? tasks.map((task) => <Task key={task.id} id={task.id} todo={task.todo} onDelete={deleteButtonHandle}/>) : null}
-  <Button onClick={addButtonHandle} variant='contained'>Add</Button>
+  { tasks ? tasks.map((task) => <Task key={task.id} id={task.id} todo={task.todo} onDelete={deleteButtonHandler}/>) : null}
+  <Button onClick={addButtonHandler} variant='contained'>Add</Button>
   <Footer/>
   </>
   
