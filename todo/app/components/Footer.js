fdbb2dd24e5d338a8da07d14204737b878e005e2
phamtrunganhwork <143469756+phamtrunganhwork@users.noreply.github.com>
2026-03-11 20:29:44 +0700

    Header Footer Placeholder

--- todo/app/components/Footer.js
+++ todo/app/components/Footer.js
@@ -1,3 +1,9 @@
-export default function Footer(){
-    return <div><p>This is a footer</p></div>
+import { Typography } from "@mui/material";
+
+export default function Footer() {
+  return <>
+  <Typography variant="body2" align="center" >
+    {"CopyWright © PTA 2026"}
+  </Typography>
+  </>;
 }
--- todo/app/components/Header.js
+++ todo/app/components/Header.js
@@ -1,7 +1,13 @@
 
+import { AppBar, Toolbar } from "@mui/material"
+import {favicon} from "../favicon.ico"
+
 export default function Header(){
-        return <>
-    <div>
-        <h1> Todo list</h1>
-    </div></>
+    return <>
+    <AppBar position='sticky' sx={{width: '100%', borderRadius: 1}}>
+        <Toolbar>
+            <favicon/>
+        </Toolbar>    
+    </AppBar>
+</>
 }
