4d5fbbfc6eda5a7b64956ebbbc9e02bbca4fe176
phamtrunganhwork <143469756+phamtrunganhwork@users.noreply.github.com>
2026-03-11 20:30:38 +0700

    Remove Modify Button

--- todo/app/components/Task.js
+++ todo/app/components/Task.js
@@ -1,25 +1,36 @@
+import {
+  Button,
+  Typography,
+  TextField,
+  Container,
+  Stack,
+  Checkbox,
+} from "@mui/material";
 
-import {Button, Typography, TextField, Container, Stack, Checkbox} from '@mui/material'
+export default function Task({ id, todo, onDelete, onModify }) {
+  const deleteButtonHandler = () => {
+    console.log(id);
+    onDelete(id);
+  };
 
-export default function Task({id, todo, onDelete, onModify}){
-
-    const deleteButtonHandler = () => {
-        console.log(id)
-        onDelete(id)
-    }
-
-    const modifyButtonHandler =() => {
-        onModify(id)
-    }
-
-    return <div>
-        <Container className="TaskWrapper">
-            <Stack className="Task" direction="row" spacing={2} margin={1}>
-                <Checkbox sx={{transform: 'scale(2)'}}/>
-                <TextField variant='outlined' />
-                <Button variant='contained' onClick={() => modifyButtonHandler()}>Modify</Button>
-                <Button variant='contained' onClick={() => deleteButtonHandler()}>Delete</Button>
-            </Stack>
-        </Container>
+  return (
+    <div>
+      <Container className="TaskWrapper">
+        <Stack
+          className="Task"
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+          margin={1}
+        >
+          <Checkbox sx={{ transform: "scale(2)" }} />
+          <TextField variant="outlined" />
+          <Button variant="contained" onClick={() => deleteButtonHandler()}>
+            Delete
+          </Button>
+        </Stack>
+      </Container>
     </div>
+  );
 }
--- todo/app/page.js
+++ todo/app/page.js
@@ -1,37 +1,52 @@
-'use client'
+"use client";
 
-import {useState} from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Task from './components/Task'
-import Button from '@mui/material/Button'
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Task from "./components/Task";
+import Button from "@mui/material/Button";
+import { Container } from "@mui/material";
 
-export default function Page(){
+export default function Page() {
+  const [tasks, setTasks] = useState([{ id: 0, todo: "One" }]);
+  const [idgiver, setIdgiver] = useState(1);
 
-  const [tasks, setTasks] = useState([{id:0, todo:"One"}]);
-  const [idgiver, setIdgiver] = useState(1)
-
-  function addButtonHandler(){
-    setIdgiver(idgiver+1)
-    setTasks([...tasks, {id: idgiver, todo: "New Task"}])
-    console.log(tasks)
+  function addButtonHandler() {
+    setIdgiver(idgiver + 1);
+    setTasks([...tasks, { id: idgiver, todo: "New Task" }]);
+    console.log(tasks);
   }
 
-  function deleteButtonHandler(id){
-    setTasks(tasks.filter((task) => task.id !== id))
-    console.log(id)
+  function deleteButtonHandler(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log(id);
   }
 
-  function modifyButtonHandler(id, todo){
-    setTasks(tasks.map((task) => task.id === id ? task.todo = todo : task));
-  }
-
-  return <>
-  <Header/>
-  {console.log(tasks)}
-  { tasks ? tasks.map((task) => <Task key={task.id} id={task.id} todo={task.todo} onDelete={deleteButtonHandler}/>) : null}
-  <Button onClick={addButtonHandler} variant='contained'>Add</Button>
-  <Footer/>
-  </>
-  
+  return (
+    <>
+      <Header />
+      <Container sx={{height: 300, overflowY: "scroll"}}>
+        {tasks
+          ? tasks.map((task) => (
+              <Task
+                key={task.id}
+                id={task.id}
+                todo={task.todo}
+                onDelete={deleteButtonHandler}
+              />
+            ))
+          : null}
+      </Container>
+      <Container>
+      <Button
+        onClick={addButtonHandler}
+        variant="contained"
+        sx={{display: "block", margin: "20px auto", width: '50%'}}
+      >
+        Add
+      </Button>
+      <Footer />
+      </Container>
+    </>
+  );
 }
